7930e2677245bdeccba4d335999a7c604b21dfe6
IRIS <irischamplin@gmail.com>
2026-07-14 17:57:20 -0500

    Update simple-footer.js

--- simple-footer/component/simple-footer.js
+++ simple-footer/component/simple-footer.js
@@ -9,8 +9,8 @@ const FOOTER_ICONS = {
   email: `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>`,
 };
 
-const TEMPLATE = document.createElement("template");
-TEMPLATE.innerHTML = `
+const FOOTER_TEMPLATE = document.createElement("template");
+FOOTER_TEMPLATE.innerHTML = `
   <style>
     :host {
       display: block;
@@ -209,7 +209,7 @@ class SimpleFooter extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(TEMPLATE.content.cloneNode(true));
+    this.shadowRoot.appendChild(FOOTER_TEMPLATE.content.cloneNode(true));
   }
 
   connectedCallback() {
